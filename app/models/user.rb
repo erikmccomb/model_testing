a75cd6a529406852586cc73845b223247cae504c
Erik McComb <erik@erikmccomb.com>
2017-09-11 10:51:06 -0600

    first and last name added to user migrate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def info
+    '#{self.email} has signed in: #{self.sign_in_count} times'
+  end
 end
