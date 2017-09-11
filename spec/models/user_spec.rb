ab8f82ee9741862c184d3fc1abb4d695bf96b6f5
Erik McComb <erik@erikmccomb.com>
2017-09-11 12:08:09 -0600

    corrected end do issues in user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   # Describe block - describing a unit test (validations, associations, class methods, etc...)
   # Context block - testing in a different context ( as user, as admin, as viewer, etc...)
-
+  before(:each) do
+    @user = User.create(email: 'test@test.com', password: 'password', first_name: 'Test', last_name: 'Tester')
+  end
 
 
   describe 'validations' do
@@ -13,30 +15,23 @@ RSpec.describe User, type: :model do
 
   describe '#info' do
     it 'returns email with dign in count' do
-      user = User.create(email: 'test@test.com', password: 'password', first_name: 'Test', last_name: 'Tester')
-      expect(user.info).to eq("#{user.email} has signed in: #{user.sign_in_count} times")
+      expect(@user.info).to eq("#{@user.email} has signed in: #{@user.sign_in_count} times")
     end
   end
 
   describe '#full_name' do
     it 'returns users name like last_name, first_name' do
-      user = User.create(email: 'test@test.com', password: 'password', first_name: 'Test', last_name: 'Tester')
-      expect(user.full_name).to eq("#{user.last_name}, #{user.first_name}")
+      expect(@user.full_name).to eq("#{@user.last_name}, #{@user.first_name}")
     end
   end
 
   describe '#display_name' do
     it 'returns users name like <first_name> <last_name>' do
-      user = User.create(email: 'test@test.com', password: 'password', first_name: 'Test', last_name: 'Tester')
-      expect(user.display_name).to eq("#{user.first_name} #{user.last_name}")
+      expect(@user.display_name).to eq("#{@user.first_name} #{@user.last_name}")
     end
   end
 
-  describe '#has_signed_in?'
-    before(:each) do
-      @user = User.create(email: 'test@test.com', password: 'password', first_name: 'Test', last_name: 'Tester')
-    end
-    
+  describe '#has_signed_in?' do
     it 'returns true if sign_in_count > 0' do
       @user.update(sign_in_count: 1)
       expect(@user.has_signed_in?).to eq(true)
@@ -45,4 +40,5 @@ RSpec.describe User, type: :model do
     it 'returns false if sign_in_count == 0' do
       expect(@user.has_signed_in?).to eq(false)
     end
- end
+  end
+end
